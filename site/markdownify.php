8a17c61069e47c9090219cbfff8ec6a42893d871
lisawilliams <lisa@placeblogger.com>
2012-04-29 13:17:47 -0400

    Including Markdownify for html > markdown conversion

---
+++ site/markdownify.php
@@ -0,0 +1,140 @@
+<?php require_once 'includes/markdownify/markdownify.php'; 
+$md = new Markdownify;
+
+$htmlInput ='<a
+href="http://theblackleatherbelt.com/wp-content/uploads/2012/04/
+devil_in_miss_jones.jpeg"><img class="alignleft size-full wp-image-2131"
+style="margin-top: 5px; margin-bottom: 5px; margin-left: 10px;
+margin-right: 10px;" title="devil_in_miss_jones"
+src="http://theblackleatherbelt.com/wp-content/uploads/2012/04/
+devil_in_miss_jones.jpeg" alt="" width="279" height="181" /></a>I lost
+my virginity in 1991, 10 years after the CDC published news of the
+emergence of an epidemic among gay men in New York and California, but
+before effective treatments for HIV/AIDS were developed.
+
+We were all having sex at Checkpoint Charlie back then: "Show me your
+paperzzz."  There were no rapid HIV tests then, either, so a visit to
+the clinic to get your blood drawn followed by a two week wait tended to
+slow down courtship rituals significantly in my circle.
+
+The atmosphere of fear and doubt cannot be underestimated: nobody seemed
+to know what was going on, but they knew it was bad and would kill you,
+but not before making you a pariah and a moral object lesson.  It was a
+sex panic of the first order: because unlike previous sex panics, it was
+about something real, and it really did kill people.  (The great heroes
+of this dark era are, of course,  <a
+href="http://youtu.be/Q__wGtL_rOM">the militant queers of ActUP</a>).
+
+The author of "<a
+href="http://www.amazon.com/The-Devil-Made-Me-Do/dp/0615199070">The
+Devil Made Me Do It</a>," Georgina Spelvin, starred in the
+groundbreaking and much-imitated porn movie "<a
+href="http://www.amazon.com/Devil-Miss-Jones-35th-Anniversary/dp/
+B001JA2W74/ref=sr_1_1?ie=UTF8&amp;qid=1335629176&amp;sr=8-1">The Devil
+and Miss Jones</a>." When I read books like this engaging memoir of
+Spelvin\'s sexual career, both private and public, it seems almost like
+science fiction to me: a completely different world, with completely
+different rules, one that I\'ll never be able to see or visit.
+
+The people in this book are so carefree with their sexuality!  I have an
+actual on-paper copy of this book, which I picked up at <a
+href="http://www.goodvibes.com/">Good Vibrations</a> in Brookline, but
+there\'s a Kindle edition now, and I\'m tempted to buy it so I can use the
+search feature: it\'s possible that the word "condom" is mentioned in
+this book, but if it is, I missed it.  (Spelvin does, however, mention
+Adult Industry Medical (AIM), which administered the testing regime for
+most porn performers in the US <a
+href="http://web.archive.org/web/20101016193546/http://latimesblogs.
+latimes.com/lanow/2010/10/wicked-pictures-suspends-production-as-
+precaution-following-new-hiv-case-in-porn-performer-.html">until
+collapsing in scandal in 2010</a>, late in the book, but that takes
+place well after she has departed from the industry).
+
+While the dalliances of Ms. Spelvin and her friends seem carefree from
+the perspective of those of us whose sex lives started in the HIV era,
+that doesn\'t mean that they were in any way cavalier; Georgina leads
+with her heart, has many crushes, a few divorces, and gets her heart
+broken on several occasions.  This is a wonderful corrective: I think
+the current sexual climate has given many people a warped view -- they
+think that those freewheeling free-love types must not have cared about
+each other very much.  Ain\'t necessarily so.
+
+Spelvin takes us through the rustic setting of the porn industry in
+those days, well before it had become the big, industrial megaplex it is
+today.  She describes seeing the set where The Devil In Miss Jones is
+filmed: <blockquote>Late in the afternoon we pull up before a looming
+nineteenth-century barn-like structure. It stands in majestic isolation
+on a slight knoll.  The last quarter of our trip had been through
+towering trees sporting vivid fall colors.  The only sign of
+civilization seen for a half-hour or more, a small roadhouse tucked in
+among the abundant foliage.  Now before us an expansive field of long
+amber grass stretches to thick woods on three sides.</blockquote> If
+that\'s not rustic enough, when Georgina and the crew get inside, they
+are jubilant to find... <blockquote>"Hallelujah!" I explode. "There is a
+God.  I found a hot water heater.  Anyone know how to light this
+thing?"</blockquote> Ms. Spelvin is not only the star of these early
+productions -- she\'s also in charge of craft services.  That\'s $100 a
+day to perform, and another $25 to cook.
+
+You really have to respect someone who can star in a porn movie and then
+feed her fellow performers and crew a standing rib roast, as Spelvin
+does on the set, having snagged food for the entire crew for $56 at
+wholesalers in her New York City neighborhood.
+
+Of course, being in the porn industry (then or now) doesn\'t offer a lot
+of job security.  Spelvin goes on to do musical theater, and encounters
+the perils of being a sexual outlaw in her era -- perils that aren\'t
+medical, but legal:
+
+<blockquote>Two men in suits -- nobody wears suits in the summer in
+Maine -- are walking slowly down the aisle [of the theater] looking very
+uncomfortable... "Are you Georgina Spelvin?" asks the taller of the two
+Suits. "On occasion," I reply -- not meaning to be sarcastic, just
+accurate. "We are here to serve you with a fugitive warrant for your
+arrest."</blockquote> The ensuing scene -- where the play\'s producer
+convinces the two FBI agents to keep an eye on Ms. Spelvin from the
+audience and take her into custody *after* the show -- is priceless.  
+The warrant turns out to be a witness warrant served by a state\'s
+attorney who apparently figures that putting porn stars on the stand
+would polish up his credentials as a true-blue moral crusader.  However,
+it\'s dropped, and Spelvin never has to testify.
+
+After that point, Spelvin seems adrift, casting from place to place in a
+school bus retrofitted with a woodstove (a vehicle that\'s like the
+Millenium Falcon in the science fictional universe known as America
+between 1950 and the election of Ronald Reagan), alighting at a clothing
+optional trailer park in the deserts just outside of San Diego.  Her
+relationship with alcohol accelerates into a real problem; she gets
+herself sober for jobs that are increasingly more difficult to come by.
+ Spelvin never blames the porn industry for her troubles, nor, indeed,
+does she seem very troubled by starring in what she and her crowd call
+"fuckfilms,"  -- in fact, many of the happiest passages of the book
+involve the laughter and camraderie of people on the sets of her movies.
+
+Stripping and live performance is another matter, however, and Spelvin
+armors herself with increasingly large amounts of alcohol before
+performances.  Her mother shows up to shepherd her through a few final
+performances in Atlantic City before taking her home to Texas to dry
+out, which she does, after a family friend who shows up at the hospital
+after her mother\'s death asks if she wants to attend an AA meeting.
+
+Ms. Spelvin ends up healthy, happy, and living under the Hollywood sign,
+bolstered by a long and happy marriage to her husband, John.  While
+there were a few points at which I didn\'t quite understand the
+transitions in this book -- wait, what year are we in? Oh! It\'s a
+flashback -- this is a lovely, comfortable, funny book.  You can find it
+-- on paper and as a Kindle book -- at Amazon.
+
+<a href="http://www.amazon.com/The-Devil-Made-Me-Do/dp/0615199070">The
+Devil Made Me Do It, by Georgina Spelvin (Amazon)</a>
+
+<a
+href="http://www.amazon.com/Devil-Miss-Jones-35th-Anniversary/dp/
+B001JA2W74/ref=sr_1_1?ie=UTF8&amp;qid=1335629176&amp;sr=8-1">The Devil
+In Miss Jones</a> is available in a 35th anniversary DVD, also from
+Amazon. Ms. Spelvin\'s blog: <a
+href="http://georginaspelvin.blogspot.com/">Georgina\'s World</a>';
+
+
+echo $md->parseString($htmlInput);
+?>
